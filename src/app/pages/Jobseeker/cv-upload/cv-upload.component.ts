b7e7ebb224a3e5c09d55f7b6ed52f71bda5ea597
hunnytyagi1996microlent <honeytyagi9045@gmail.com>
2024-12-25 17:41:35 +0530

    implement image upload,  size, extenstion functionality in image uplaod service  some changes in cv upload component

--- src/app/pages/Jobseeker/cv-upload/cv-upload.component.ts
+++ src/app/pages/Jobseeker/cv-upload/cv-upload.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ImageCompressionService } from '../../../core/services/image-compression.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormControl,Validators } from '@angular/forms';
-
+import { ImageUploadService } from '../../../core/services/image-upload.service';
 @Component({
   selector: 'app-cv-upload',
   standalone: true,
@@ -15,28 +15,46 @@ export class CvUploadComponent {
   compressedImage: string | null = null;
   cvFile: File | null = null;
 
-  constructor(private imageCompressionService: ImageCompressionService) {
+  constructor(private imageCompressionService: ImageCompressionService,
+              private imageUploadService : ImageUploadService ) {
     this.cvForm = new FormGroup({
       name: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       phone: new FormControl('', Validators.required),
     });
   }
-
   onImageSelect(event: Event): void {
-    const file = (event.target as HTMLInputElement).files?.[0];
-    if (file) {
+    try {
+      const file = this.imageUploadService.validateImage(event, 5); // Validate image with a max size of 5MB
       this.imageCompressionService
-        .compressImage(file, 1)
+        .compressImage(file, 0.8) // Compress the image to 80% quality
         .then((compressedImage) => {
           this.compressedImage = compressedImage;
-          console.log('Compressed Image:', compressedImage);
+          const compressedBlob = this.dataURLToBlob(compressedImage);
+          console.log('Compressed Image Size (in bytes):', compressedBlob.size);
         })
         .catch((error) => {
           console.error('Image Compression Error:', error);
         });
+    } catch (error:any) {
+      console.error('Image Validation Error:', error.message);
     }
   }
+
+  // onImageSelect(event: Event): void {
+  //   const file = (event.target as HTMLInputElement).files?.[0];
+  //   if (file) {
+  //     this.imageCompressionService
+  //       .compressImage(file, 1)
+  //       .then((compressedImage) => {
+  //         this.compressedImage = compressedImage;
+  //         console.log('Compressed Image:', compressedImage);
+  //       })
+  //       .catch((error) => {
+  //         console.error('Image Compression Error:', error);
+  //       });
+  //   }
+  // }
 
   onCvSelect(event: Event): void {
     const file = (event.target as HTMLInputElement).files?.[0];
