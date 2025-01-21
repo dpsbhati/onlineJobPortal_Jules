e73ad7875d8ed73f04b918b1aed924e5b07eebaf
Ankita David <ankita.microlent@gmail.com>
2025-01-21 15:34:59 +0530

    implement functionality og job-list and edit-profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
 import { Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: '/authentication/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     component: FullComponent,
+    canActivate: [AuthGuard],
     children: [
-      {
-        path: '',
-        redirectTo: '/starter',
-        pathMatch: 'full',
-      },
       {
         path: 'starter',
         loadChildren: () =>
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { StarterComponent } from './starter/starter.component';
+import { EditProfileComponent } from './user/edit-profile/edit-profile.component';
+import { AuthGuard } from '../core/guards/auth.guard';
+import { JobListComponent } from './admin/job-list/job-list.component';
 
 export const PagesRoutes: Routes = [
   {
@@ -11,6 +14,26 @@ export const PagesRoutes: Routes = [
         { title: 'Applications', url: '/starter' },
         { title: 'Applications List' },
       ],
+    },
+  },
+  {
+    path: 'edit-profile',
+    component: EditProfileComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Edit Profile',
+      // urls: [
+      //   { title: 'Dashboard', url: '/starter' },
+      //   { title: 'Edit Profile' },
+      // ],
+    },
+  },
+  {
+    path: 'job-list',
+    component: JobListComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Job Listings',
     },
   },
 ];
