1f1d82940a5f2898076caea85966118b2cd58590
hunnytyagi1996microlent <honeytyagi9045@gmail.com>
2025-01-22 16:56:41 +0530

    pages-route

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,48 +4,10 @@ import { EditProfileComponent } from './user/edit-profile/edit-profile.component
 import { AuthGuard } from '../core/guards/auth.guard';
 import { JobListComponent } from './admin/job-list/job-list.component';
 import { CreateJobPostingComponent } from './admin/create-job-posting/create-job-posting.component';
-import { Application, ApplicationsComponent } from './admin/applications/applications.component';
 
 export const PagesRoutes: Routes = [
   {
     path: '',
-    component: StarterComponent,
-    data: {
-      title: 'Administration',
-      urls: [
-        { title: 'Applications', url: '/starter' },
-        { title: 'Applications List' },
-      ],
-    },
-  },
-  {
-    path: 'create-job-posting',
-    component : CreateJobPostingComponent
-  },
-   {
-    path: 'applications',
-    component : ApplicationsComponent
-  },
-  {
-    path: 'edit-profile',
-    component: EditProfileComponent,
-    canActivate: [AuthGuard],
-    data: {
-      title: 'Edit Profile',
-      // urls: [
-      //   { title: 'Dashboard', url: '/starter' },
-      //   { title: 'Edit Profile' },
-      // ],
-    },
-  },
-  {
-    path: 'job-list',
-    component: JobListComponent,
-    canActivate: [AuthGuard],
-    data: {
-      title: 'Job Listings',
-    },
-  },
     children: [
       {
         path: 'starter',
