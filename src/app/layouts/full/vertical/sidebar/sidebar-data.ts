de5438b27756525270595c565786d40003cc8457
Muskan Vaishnav <muskan.vaishnav@microlent.com>
2025-05-20 17:37:05 +0530

    issue resolve

--- src/app/layouts/full/vertical/sidebar/sidebar-data.ts
+++ src/app/layouts/full/vertical/sidebar/sidebar-data.ts
@@ -37,7 +37,7 @@ export const navItems: NavItem[] = [
     visibleForRoles: ['applicant']
   },
    {
-    displayName: 'List of Jobs',
+    displayName: 'Job List',
     iconName: 'briefcase',
     route: '/',
     visibleForRoles: ['applicant']
