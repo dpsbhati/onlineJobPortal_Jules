e73bb7fd2aefdefa74405c131727f145fd4490a1
harshitmathur_microlent <harshit.mathur@microlent.com>
2025-05-29 14:44:37 +0530

    changes in application

--- src/application/application.service.ts
+++ src/application/application.service.ts
@@ -76,6 +76,7 @@ export class ApplicationService {
       const jobDetails = await this.applicationRepository
         .createQueryBuilder('app')
         .leftJoinAndSelect('app.job', 'job')
+        .leftJoinAndSelect('job.ranks', 'ranks')
         .leftJoinAndSelect('app.user', 'user')
         .where('app.id = :id', { id: savedApplication.id })
         .getOne();
@@ -95,7 +96,7 @@ export class ApplicationService {
       console.log('adminUserIds', adminUserIds);
 
       const notificationSubject = 'New Job Application Received';
-      const notificationContent = `${req.user.userProfile.first_name} ${req.user.userProfile.last_name} has applied for the job of ${jobDetails.job.rank}.`;
+      const notificationContent = `${req.user.userProfile.first_name} ${req.user.userProfile.last_name} has applied for the job of ${jobDetails.job.ranks.rank_name}.`;
 
       // Send notification to all admins
       this.notificationGateway.emitNotificationToUsers(
