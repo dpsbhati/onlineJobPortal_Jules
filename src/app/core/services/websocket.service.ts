3436c66017fab4970aac942ca64339a3a765ff11
Ankita David <ankita.microlent@gmail.com>
2025-05-30 14:45:53 +0530

    complete redirection work in websocket, header, notifications

--- src/app/core/services/websocket.service.ts
+++ src/app/core/services/websocket.service.ts
@@ -1,44 +1,103 @@
 // socket.service.ts
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import * as io from 'socket.io-client';
+import { AdminService } from './admin/admin.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
+  currentUserId: any;
+  notificationlist: any;
+  total: any;
+  pageConfig: any = {
+    curPage: 1,
+    perPage: 10,
+    sortBy: 'created_at',
+    direction: 'desc',
+    whereClause: [],
+  };
   private socket: io.Socket;
   loginUserId: any;
+  private notificationListSubject = new BehaviorSubject<any[]>([]);
+  public notificationList$ = this.notificationListSubject.asObservable();
 
+  private totalSubject = new BehaviorSubject<number>(0);
+  public total$ = this.totalSubject.asObservable();
+  
   constructor(
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private adminService: AdminService,
+
   ) {
     const userString = localStorage.getItem('user');
     this.loginUserId = userString ? JSON.parse(userString) : null;
+    if (userString) {
+      try {
+        const user = JSON.parse(userString);
+        this.currentUserId = user.id;
+      } catch (error) {
+        this.currentUserId = null;
+      }
+    }
     console.log(this.loginUserId);
-    
+
   }
 
   connect(url: string): void {
     console.log(url)
     this.socket = io.io(url, {
-      transports:['websocket', 'polling'],
-      withCredentials:true,
+      transports: ['websocket', 'polling'],
+      withCredentials: true,
       query: { userId: this.loginUserId?.id }
     });
     console.log(this.socket);
-    
+
   }
 
   listen(eventName: string, params?: any): Observable<any> {
     return new Observable(observer => {
-      this.socket.on(eventName, (data: any) =>{
+      this.socket.on(eventName, (data: any) => {
         console.log(data)
         observer.next({ data, params })
-        this.toastr.info(data.message, data.title, {timeOut:10000})
+        this.toastr.info(data.message, data.title, { timeOut: 10000 });
+         observer.next({ data, params });
+        this.onPagination();
       });
       return () => this.socket.off(eventName);
+    });
+  }
+
+   onPagination(): void {
+    if (!this.currentUserId) {
+      this.notificationListSubject.next([]);
+      this.totalSubject.next(0);
+      return;
+    }
+    this.pageConfig.whereClause = [
+      {
+        key: 'user_id',
+        value: this.currentUserId,
+        operator: '=',
+      },
+    ];
+    this.adminService.notificationPagination(this.pageConfig).subscribe({
+      next: (res: any) => {
+        if (res.statusCode === 200) {
+          this.notificationListSubject.next(res.data);
+          this.totalSubject.next(res.count || 0);
+        } else {
+          this.notificationListSubject.next([]);
+          this.totalSubject.next(0);
+        }
+      },
+      error: (err: any) => {
+        this.toastr.error(err?.error?.message);
+        this.notificationListSubject.next([]);
+        this.totalSubject.next(0);
+      },
     });
   }
 
