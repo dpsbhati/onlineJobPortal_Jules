1d90d8df01ad4e443cb66bc298ca0e1d8fed1d1f
Habib74 <habeeb@microlent.com>
2025-06-11 15:42:08 +0530

    issue

--- src/user-profile/entities/training-certificate.entity.ts
+++ src/user-profile/entities/training-certificate.entity.ts
@@ -1,26 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Generated,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('travel_documents')
 export class TrainingCertificate {
-@Column()
-   traning_type_id?: string;
- 
-   @Column()
-   user_id?: string;
- 
- 
- @Column()
-   certificate_type?: string;
- 
- @Column()
-   document_number?: string;
- 
- @Column()
-   issue_place?: string;
- 
- @Column()
-   issue_date?: Date;
- 
- @Column()
-   exp_date?: Date;
+  @PrimaryColumn({ type: 'uuid' })
+  @Generated('uuid')
+  id: string;
+  @Column()
+  traning_type_id?: string;
+
+  @Column()
+  user_id?: string;
+
+  @Column()
+  certificate_type?: string;
+
+  @Column()
+  document_number?: string;
+
+  @Column()
+  issue_place?: string;
+
+  @Column()
+  issue_date?: Date;
+
+  @Column()
+  exp_date?: Date;
 }
